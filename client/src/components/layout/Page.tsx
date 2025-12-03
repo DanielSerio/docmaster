7e241af688016e37c600afc149da08e78e0b49dc
DanielSerio <dserio.d@gmail.com>
2025-12-03 11:36:55 -0500

    feat: Establish initial UI structure with Header and Page components, integrating Tailwind CSS and theming.

--- client/src/components/layout/Page.tsx
+++ client/src/components/layout/Page.tsx
@@ -1,11 +1,14 @@
-import type { HTMLAttributes } from "react";
+import type { HTMLAttributes } from 'react';
 
-export interface PageProps extends Omit<HTMLAttributes<HTMLAreaElement>, 'id'> { }
+export interface PageProps extends Omit<HTMLAttributes<HTMLAreaElement>, 'id'> {}
 
 export function Page({ children, ...props }: PageProps) {
   return (
-    <main id="page" {...props}>
-      {children}
+    <main
+      id="page"
+      {...props}
+    >
+      <section className="container mx-auto px-4">{children}</section>
     </main>
   );
 }
